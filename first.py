04167c95ccbdc2cc9a515d0d4ad6e811a1f690f7
Kaleb Franken <kaleb.franken7@gmail.com>
2022-12-06 21:13:02 -0700

    function works

--- first.py
+++ first.py
@@ -1,5 +1,10 @@
-print('hello world')
-print(5) # first comment!
-type(int):
-	print(1/3)
-end
+def fib(n):    # write Fibonacci series up to n
+     """Print a Fibonacci series up to n."""
+     a, b = 0, 1
+     while a < n:
+         print(a, end=' ')
+         a, b = b, a+b
+     print()
+
+# Now call the function we just defined:
+fib(2000)
